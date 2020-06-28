52eecc7373dcaae03a97058163c6fd105cf75134
Mateusz Kaleta <m.kaleta9696@gmail.com>
2020-06-28 22:10:08 +0200

    Updates

---
+++ precache-manifest.b44cfe464835f009679d7c950d9f5f2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554070adb7c6d6d77e5a1effbf831c9f",
+    "url": "/app-weather/index.html"
+  },
+  {
+    "revision": "c2200fde1b90a3e6082b",
+    "url": "/app-weather/static/css/main.8c6bb883.chunk.css"
+  },
+  {
+    "revision": "579b187fb39a3303c55c",
+    "url": "/app-weather/static/js/2.b5236c29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-weather/static/js/2.b5236c29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2200fde1b90a3e6082b",
+    "url": "/app-weather/static/js/main.4d32a399.chunk.js"
+  },
+  {
+    "revision": "f6a167d0f614bbdac1c8",
+    "url": "/app-weather/static/js/runtime-main.3d596684.js"
+  },
+  {
+    "revision": "240c2422388245800edcc778757e3741",
+    "url": "/app-weather/static/media/science_1920.240c2422.jpg"
+  }
+]);
